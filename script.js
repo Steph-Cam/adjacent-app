bf798994adf94fc42404125825a3e919154ab740
cameronduffin <camduffin@gmail.com>
2021-02-20 17:01:38 -0500

    beginning to work through api data

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
+const artistApp = {};
 
-const url = new URL('https://ws.audioscrobbler.com/2.0/');
-url.search = new URLSearchParams({
-    method: 'artist.getSimilar',
-    artist: 'kayne',
-    api_key: 'e35219e4872d0f1dcee255f3768714bb',
-    format: 'json'
-})
+artistApp.apiUrl = "https://ws.audioscrobbler.com/2.0/";
+artistApp.apiKey = "e35219e4872d0f1dcee255f3768714bb";
 
-fetch(url)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data)
-})
+artistApp.getArtists = () => {
+
+    const url = new URL(artistApp.apiUrl);
+    url.search = new URLSearchParams({
+        method: 'artist.getSimilar',
+        artist: 'kanye',
+        api_key: artistApp.apiKey,
+        format: 'json',
+        limit: 9,
+        // autocorrect: [1]
+    })
+    
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        artistApp.displayArtists(data);
+    })
+}
+
+artistApp.displayArtists = (artistData) => {
+
+    const ulElement = document.querySelector('ul').innerHTML = '';
+
+    artistData.forEach((titleData) => {
+        const listElement = document.createElement('li');
+
+        const artistTitle = document.createElement('h2');
+
+        artistTitle.textContent = titleData.similarartists[artist].name;
+
+        // titleData.artist[0].name
+
+        
+        // listElement.appendChild(artistTitle);
+        
+        // ulElement.appendChild(listElement);
+        
+        // console.log(artistTitle);
+    });
+
+}
+
+
+// create init method
+artistApp.init = () => {
+    artistApp.getArtists();
+}
+
+artistApp.init();
 
 
 
-
-
-// const url = new URL('http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar');
-
-
-// url.search = new URLSearchParams({
-//     limit: 9,
-//     artist: '',
-//     match: 1,
-//     api_key:'e35219e4872d0f1dcee255f3768714bb',
-// })
-
-// // console.log(url);
-
-// fetch(url)
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function (jsonResponse) {
-//     console.log(jsonResponse)
-// })

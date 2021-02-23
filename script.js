47c5ef8965c41902bf8dc4c5f7c10579dd0ff8ce
cameronduffin <camduffin@gmail.com>
2021-02-23 18:29:16 -0500

    adding second API to access artist images

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 const artistApp = {};
 
+// LastFM API
 artistApp.apiUrl = "https://ws.audioscrobbler.com/2.0/";
 artistApp.apiKey = "e35219e4872d0f1dcee255f3768714bb";
 
+// AudioDB API
+artistApp.apiUrl2 = "https://www.theaudiodb.com/api/v1/json/1/search.php";
+
+
 const ulElement = document.getElementById("newArtists");
 
+// AudioDB API 
+artistApp.getPhotos = (artist) => {
+  const url = new URL(artistApp.apiUrl2);
+  url.search = new URLSearchParams({
+    s: artist 
+  })
+  
+  fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data2) {
+    console.log(data2);
+    
+  })
+  
+  // 1 take the data from array of new artists from first API call 
+  // 2 pass those artists into a second API call to retreive images for those artists 
+  // display images in ul on the page
+
+
+
+
+  // const artistArray = artistData.similarartists.artist;
+  // console.log(artistArray);  
+  
+}
+
+// LastFM API 
 artistApp.getArtists = (artist) => {
 
     const url = new URL(artistApp.apiUrl);
@@ -16,116 +50,101 @@ artistApp.getArtists = (artist) => {
         limit: 9,
         // autocorrect: [1]
     })
-    
+
     fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-      // console.log(data);
-      // console.log(data.similarartists.artist);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+            // console.log(data.similarartists.artist);
 
-      artistApp.displayArtists(data);
-    })
+            artistApp.displayArtists(data);
+        })
 
-  
 }
 
 artistApp.displayArtists = (artistData) => {
-// console.log(artistData);
+    // console.log(artistData);
 
-const artistArray = artistData.similarartists.artist;
-console.log(artistArray);
+    const artistArray = artistData.similarartists.artist;
+    console.log(artistArray);
 
     artistArray.forEach((item) => {
-      // console.log(item.name);
+        // console.log(item.name);
 
-      const listElement = document.createElement("li");
-      // const searchArtist = document.createElement("h2")
-      const artistName = document.createElement("h3");
-      const artistButton = document.createElement("a");
+        const listElement = document.createElement("li");
+        // const searchArtist = document.createElement("h2")
+        const artistName = document.createElement("h3");
+        const artistButton = document.createElement("a");
 
-      artistName.textContent = item.name;
-      // first attemp:
-    //   artistButton.setAttribute("href", item.url);
-      artistButton.setAttribute('href', item.url);
+        artistName.textContent = item.name;
+        // first attemp:
+        //   artistButton.setAttribute("href", item.url);
+        artistButton.setAttribute('href', item.url);
 
-      // Cam's probably sketchy way: 
-      // artistButton.innerText = 'Artist Profile';
+        // Cam's probably sketchy way: 
+        // artistButton.innerText = 'Artist Profile';
 
-      // Susan's way:
-      artistButton.append('Artist Profile');
+        // Susan's way:
+        artistButton.append('Artist Profile');
 
+        // console.log(artistName);
+        // console.log(artistButton);
 
+        listElement.appendChild(artistName);
+        listElement.appendChild(artistButton);
 
-      // console.log(artistName);
-      // console.log(artistButton);
-  
-      listElement.appendChild(artistName);
-      listElement.appendChild(artistButton);
-
-      //  add list elements to the ul
-      ulElement.appendChild(listElement);
+        //  add list elements to the ul
+        ulElement.appendChild(listElement);
     });
 
 }
 
 artistApp.getUserInput = () => {
 
-  const formElement = document.querySelector("form");
+    const formElement = document.querySelector("form");
 
-  // attach event listener to get info on submit
-  formElement.addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    // console.log(e);
+    // attach event listener to get info on submit
+    formElement.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    // clear out results section before adding new results
-    ulElement.innerHTML = "";
-    
-    // get the value of what the user submits in form
-    // const artist = formElement.value;
-    const artist = e.target[0].value;
-    console.log(artist);
-    
-    
-    // clear out the search field upon submission
-    // call getArtists function by passing in the value
-    artistApp.getArtists(artist);
-    document.getElementById("artistInput").value = "";
-  });
-  
+        // console.log(e);
+
+        // clear out results section before adding new results
+        ulElement.innerHTML = "";
+
+        // get the value of what the user submits in form
+        // const artist = formElement.value;
+        const artist = e.target[0].value;
+        console.log(artist);
+
+        // alert user if they enter an empty string
+        if (artist === '') {
+            alert('please enter an artist')
+        };
+        
+        // const artistResults = e.artists[0].strArtist;
+        // console.log(artistResults);
+
+        // clear out the search field upon submission
+        // call getArtists function by passing in the value
+        artistApp.getPhotos(artist);
+        artistApp.getArtists(artist);
+        document.getElementById("artistInput").value = "";
+    });
+
 
 }
+
+
 
 
 // create init method
 artistApp.init = () => {
-  artistApp.getUserInput();
-  // artistApp.getArtists();
+    artistApp.getUserInput();
+    // artistApp.getArtists();
 }
 
 artistApp.init();
-
-
-
-// formElement.addEventListener("submit", function(e) {
-//   e.preventDefault();
-  
-//   console.log(e);
-  
-//   // clear out results section before adding new results
-//   ulElement.innerHTML = "";
-  
-//   // get the value of what the user submits in form
-//   // const artist = formElement.value;
-//   const artist = e.target;
-//   console.log(artist);
-  
-  
-//   document.getElementById("artistInput").value = "";
-//   // clear out the search field upon submission
-//   // call getArtists function by passing in the value
-//   artistApp.getArtists(artist);
-// });
 

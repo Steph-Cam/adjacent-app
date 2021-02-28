2a8158b6eabfb56771e6c5ee3d2faa1168706740
stephp <stephanie.penev@gmail.com>
2021-02-28 16:21:18 -0500

    finished media queries and additional code cleanup

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const artistApp = {};
+
 // LastFM API
 artistApp.apiUrl = "https://ws.audioscrobbler.com/2.0/";
 artistApp.apiKey = "e35219e4872d0f1dcee255f3768714bb";
+
 // AudioDB API
 artistApp.apiUrl2 = "https://www.theaudiodb.com/api/v1/json/1/search.php";
 const ulElement = document.getElementById("newArtists");
-// const url2 = new URL(artistApp.apiUrl2);
-// url2.search = new URLSearchParams({
-//   s: "cave in",
-// });
+
 let newArray = [];
 artistApp.getArtists = (artist) => {
   
@@ -19,7 +18,7 @@ artistApp.getArtists = (artist) => {
     api_key: artistApp.apiKey,
     format: "json",
     limit: 4,
-    // autocorrect: [1]
+    autocorrect: [1]
   });
   fetch(url1)
     .then((response) => {
@@ -31,11 +30,8 @@ artistApp.getArtists = (artist) => {
       const dataArray = [];
       for (let i = 0; i < artists.length; i++) {
         dataArray.push(artists[i].name);
-        // dataArray.push(artists[i].url);
       }
       
-      // similar to the pokemon example where we had a array of URLS, you now have an array of artist names
-      // that can be mapped over to send a fetch request for each one
       const newArtists = dataArray.map((artistImage) => {
         
         return fetch(
@@ -46,7 +42,6 @@ artistApp.getArtists = (artist) => {
       });
       
       Promise.all(newArtists).then((secondData) => {
-        // console.log(newArtists);
         const artistsToDisplay = [];
       
         secondData.forEach((secondDataChild) => {
@@ -56,29 +51,21 @@ artistApp.getArtists = (artist) => {
               imgThumb: artistThumb.strArtistThumb,
               url: `http://${artistThumb.strWebsite}`,
             });
-            // newArray.push(artistThumb.strArtistThumb + artistThumb.strArtist);
-            
           });
-          // secondData[0].artists.map((artistThumb) => {
-          //     newArray.push(artistThumb.strArtistThumb);
         });
-        console.log(artistsToDisplay);
         artistApp.displayArtists(artistsToDisplay);
-      });
-      
-      // dataArray.push(newArtists);
-  
+      })
+      .catch(error => {
+        alert('Sorry, no artist found!');
+      })
     });
-  // artistApp.displayArtists(newArray);
-  // return newArray;
 };
-// artistApp.getArtists();
-// Display Artists function
+
 artistApp.displayArtists = (artistArray) => {
-  // array of objects is looped over
+
   ulElement.innerHTML = "";
   artistArray.forEach((item) => {
-    // create these elements
+
     const listElement = document.createElement("li");
     const artistImage = document.createElement("img");
     const artistName = document.createElement("h3");
@@ -87,57 +74,30 @@ artistApp.displayArtists = (artistArray) => {
     artistImage.setAttribute("alt", `${item.name} Image`);
     artistButton.setAttribute("target", '_blank');
     artistButton.setAttribute('rel', 'noopener');
-
-    artistName.textContent = item.name;
     artistButton.setAttribute("href", item.url);
-  
-    artistButton.append("Artist Profile");
-    //  add list elements to the ul
+    artistName.textContent = item.name;
+    artistButton.append("Artist Website");
     listElement.appendChild(artistButton);
     listElement.appendChild(artistName);
     listElement.appendChild(artistImage);
     ulElement.appendChild(listElement);
   });
 };
+
 artistApp.getUserInput = () => {
   
   const formElement = document.querySelector("form");
-  // let artist = "";
-  // attach event listener to get info on submit
+
   formElement.addEventListener("submit", function (e) {
     e.preventDefault();
-    
     artist = e.target[0].value;
-    // console.log(artist);
-    // console.log(e);
-    // clear out results section before adding new results
-    
-    // get the value of what the user submits in form
-    // ulElement.innerHTML = "";
-    // const artist = formElement.value;
-    // // alert user if they enter an empty string
-    // // const artistResults = e.artists[0].strArtist;
-    // // console.log(artistResults);
-    // // clear out the search field upon submission
-    // // call getArtists function by passing in the value
-
     artistApp.getArtists(artist);
-    // console.log(artistObjects);
-    // artistApp.displayArtists(artistObjects);
     document.getElementById("artistInput").value = "";
-    // console.log(artist);
   });
-  // console.log(artist);
-  // return artist;
 };
-// create init method
+
 artistApp.init = () => {
   artistApp.getUserInput();
-  // console.log(artist);
-  // artistApp.displayArtists();
-  // if (artist.length > 0) {
-  //   console.log(artist);
-  //   artistApp.getArtists(artist);
-  // };
 };
+
 artistApp.init();
